3917dde945bc723d02f5c8244f1424ef8c6b7728
3400952004 <81766748+3400952004@users.noreply.github.com>
2023-09-12 20:20:37 +0800

    Update effect.js

--- effect.js
+++ effect.js
@@ -11,10 +11,11 @@ $('document').ready(function(){
 			$('#b22').animate({top:240, left: vw-250},500);
 			$('#b33').animate({top:240, left: vw-150},500);
 			$('#b44').animate({top:240, left: vw-50},500);
+			$('#b88').animate({top:240, left: vw-0},500);
 			$('#b55').animate({top:240, left: vw+50},500);
 			$('#b66').animate({top:240, left: vw+150},500);
 			$('#b77').animate({top:240, left: vw+250},500);
-			$('#b88').animate({top:240, left: vw+350},500);
+			
 		
 		});
 
