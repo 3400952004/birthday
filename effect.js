62c45d0932a3e84e850c1c15be8bf7240c222a0f
3400952004 <81766748+3400952004@users.noreply.github.com>
2023-09-12 12:27:03 +0800

    Update effect.js

--- effect.js
+++ effect.js
@@ -182,7 +182,7 @@ $('document').ready(function(){
 			i=i+1;
 			$("p:nth-child("+i+")").fadeIn('slow').delay(1500);
 			if(i==50){
-				$("p:nth-child(49)").fadeIn('slow').promise().done(function () {
+				$("p:nth-child("+i-1+")").fadeIn('slow').promise().done(function () {
 					$('.cake').fadeIn('fast');
 					// 测试结尾句
 				});
